3a06d76ca3b9e07e6e9b65b378aa4698a436faeb
Mouad Kabiri <m.kabiri@aui.ma>
2022-12-28 18:22:27 +0100

    extracts information after searching

--- stalk.py
+++ stalk.py
@@ -5,13 +5,82 @@ import json
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 
 driver_path = r"C:\chromedriver.exe" 
 
 
 load_dotenv()
-EMAIL = os.getenv('EMAIL')
+AUI_ID = os.getenv('AUI_ID')
 PASSWORD = os.getenv('PASSWORD')
 WEBDRIVER_PATH = os.getenv('WEBDRIVER_PATH')
 
-driver = webdriver.Chrome(WEBDRIVER_PATH)
+
+def login(driver, EMAIL, PASSWORD):
+    driver.get("https://my.aui.ma/ICS/")
+    driver.set_window_size(1552, 832)
+    driver.find_element(By.ID, "userName").click()
+    driver.find_element(By.ID, "userName").send_keys(EMAIL)
+    driver.find_element(By.ID, "password").send_keys(PASSWORD)
+    driver.find_element(By.ID, "siteNavBar_btnLogin").click()
+
+def search_for_course(driver, course_code):
+    driver.find_element(By.LINK_TEXT, "Students").click()
+    # 12 | click | id=pg1_V_lblAddDrop | 
+    driver.find_element(By.ID, "pg1_V_lblAddDrop").click()
+    # 13 | click | id=pg0_V_ddlTerm | 
+    driver.find_element(By.ID, "pg0_V_ddlTerm").click()
+    # 14 | select | id=pg0_V_ddlTerm | label=2022-2023 Academic Year - Spring Semester
+    dropdown = driver.find_element(By.ID, "pg0_V_ddlTerm")
+    dropdown.find_element(By.XPATH, "//option[. = '2022-2023 Academic Year - Spring Semester']").click()
+    # 15 | click | id=pg0_V_tabSearch_txtCourseRestrictor | 
+    driver.find_element(By.ID, "pg0_V_tabSearch_txtCourseRestrictor").click()
+    # 16 | type | id=pg0_V_tabSearch_txtCourseRestrictor | csc 3326
+    driver.find_element(By.ID, "pg0_V_tabSearch_txtCourseRestrictor").send_keys(course_code)
+    # 17 | click | id=pg0_V_tabSearch_btnSearch | 
+    driver.find_element(By.ID, "pg0_V_tabSearch_btnSearch").click()
+
+
+def extract_table(driver):
+    # Find the tbody element that contains the rows of the table
+    tbody = driver.find_element(By.CSS_SELECTOR, 'tbody.gbody')
+
+    # Find all of the rows in the table
+    rows = tbody.find_elements(By.TAG_NAME, 'tr')
+
+    # Loop through the rows
+    for row in rows:
+        # Find the cells in the row
+        cells = row.find_elements(By.TAG_NAME, 'td')
+
+        # rows that contain 11 cells are the ones we want
+        if len(cells) != 11:
+            continue
+
+        # Extract the information from the cells
+        course_code = cells[2].text
+        professor = cells[4].text
+        seats_open = cells[5].text
+        
+        # Print the information
+        print(f'Course code: {course_code} | Professor: {professor} | Seats open: {seats_open}')
+
+    print("Done Extracting...")
+
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome(WEBDRIVER_PATH)
+    login(driver, AUI_ID, PASSWORD)
+    print("Logged in successfully")
+    print("Searching for course")
+    search_for_course(driver, "CSC 3326")
+    extract_table(driver)
+
+
+    driver.close()
