05728463f96cc7d98ecfbef2f9b6b1a457160682
essfadi <o.essfadi@aui.ma>
2022-12-29 13:18:25 +0100

    fix: omit infinte loop of message

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 token = os.getenv('BOT_TOKEN')
 
-async def send_message(message, user_message, is_priv):
+async def send_message(message: str, user_message: str, is_priv: bool):
     try:
         response = handle_response(user_message)
         await message.author.send(response) if is_priv else await message.channel.send(response)
@@ -18,7 +18,25 @@ def run_discord_bot():
     @client.event
     async def on_ready():
         print(f'{client.user} is running!')
-    client.run(token)
+    
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+    # Get data about the user
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        # Debug printing
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        # If the user message contains a '?' in front of the text, it becomes a private message
+        if user_message[0] == '?':
+            user_message = user_message[1:]  # [1:] Removes the '?'
+            await send_message(message, user_message, True)
+        else:
+            await send_message(message, user_message, False)
 
 def handle_response(message:str) -> str:
     p_message = message.lower()
